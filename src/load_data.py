5eecf9955b0b1b4c9b208a021a40515f57736446
Ethan Harvey <ethanjharvey06@gmail.com>
2026-06-29 13:10:15 +1000

    Added loading data for particular format and renamed columns for simplicity

--- src/load_data.py
+++ src/load_data.py
@@ -7,19 +7,39 @@ def load_data() -> pd.DataFrame:
         pd.DataFrame: The loaded data.
     """
     # load data
-    df = pd.read_csv('data/household_power_consumption.txt',
-                    sep=';', # separator
-                    low_memory=False, # don't load the data into memory
-                    na_values=['?']) # replace '?' with NaN
+    df = pd.read_csv('../data/Bd18850_2021100100.csv', low_memory=False)
 
-    # convert date and time to datetime (adjust for your format)
-    df['datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], 
-                                    format='%d/%m/%Y %H:%M:%S')
+    # Formatting and renaming columns
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%d/%m/%Y %H:%M:%S.%f')
+    new_names = {'[10772] NOJA_AUSNET!BD18850:FREQ' : 'Frequency',
+            '[10773] NOJA_AUSNET!BD18850:DFDT' : 'DF/DT',
+            '[10774] NOJA_AUSNET!BD18850:FLAG' : 'GPS_Flag',
+            '[10775] NOJA_AUSNET!BD18850-UA:MAG' : 'VA_Mag',
+            '[10776] NOJA_AUSNET!BD18850-UA:ANG' : 'VA_Ang',
+            '[10777] NOJA_AUSNET!BD18850-UB:MAG' : 'VB_Mag',
+            '[10778] NOJA_AUSNET!BD18850-UB:ANG' : 'VB_Ang',
+            '[10779] NOJA_AUSNET!BD18850-UC:MAG' : 'VC_Mag',
+            '[10780] NOJA_AUSNET!BD18850-UC:ANG' : 'VC_Ang',
+            '[10781] NOJA_AUSNET!BD18850-UR:MAG' : 'VR_Mag',
+            '[10782] NOJA_AUSNET!BD18850-UR:ANG' : 'VR_Ang',
+            '[10783] NOJA_AUSNET!BD18850-US:MAG' : 'VS_Mag',
+            '[10784] NOJA_AUSNET!BD18850-US:ANG' : 'VS_Ang',
+            '[10785] NOJA_AUSNET!BD18850-UT:MAG' : 'VT_Mag',
+            '[10786] NOJA_AUSNET!BD18850-UT:ANG' : 'VT_Ang',
+            '[10787] NOJA_AUSNET!BD18850-IA:MAG' : 'IA_Mag',
+            '[10788] NOJA_AUSNET!BD18850-IA:ANG' : 'IA_Ang',
+            '[10790] NOJA_AUSNET!BD18850-IB:MAG' : 'IB_Mag',
+            '[10791] NOJA_AUSNET!BD18850-IB:ANG' : 'IB_Ang',
+            '[10792] NOJA_AUSNET!BD18850-IC:MAG' : 'IC_Mag',
+            '[10793] NOJA_AUSNET!BD18850-IC:ANG' : 'IC_Ang',
+            '[10794] NOJA_AUSNET!BD18850-IN:MAG' : 'IN_Mag',
+            '[10795] NOJA_AUSNET!BD18850-IN:ANG' : 'IN_Ang'
+            }
 
-    # set datetime as index
-    df = df.set_index('datetime')
+    df.rename(columns=new_names, inplace=True)
 
-    # drop original date and time columns
-    df = df.drop(columns=['Date', 'Time'])
+
+    df.set_index('Timestamp', inplace=True)
+    df.drop(columns=['[10754] NOJA_AUSNET!BD18850:ALOG1', '[10755] NOJA_AUSNET!BD18850:ALOG2'], inplace=True)
 
     return df
