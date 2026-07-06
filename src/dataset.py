2cb07d1aaccb24e889f730dc7d2c1af9e63f9097
Ethan Harvey <ethanjharvey06@gmail.com>
2026-07-06 12:47:35 +1000

    Imnplemented functions in PMUDataset class, including add_dataframe method

--- src/dataset.py
+++ src/dataset.py
@@ -6,12 +6,17 @@ import numpy as np
 import pandas as pd
 
 class PMUDataset(Dataset):
-    def __init__(self, n_sources: int, window_size: int, stride: int) -> None:
+    def __init__(self, window_size: int, stride: int) -> None:
         """
         Initialises dataset arrays and size, window size and stride
         Includes mapping between PMU_Id and integer
         """
-        return
+        self.num_dataframes = 0
+        self.pmu_source = {}
+        self.window_size = window_size
+        self.stride = stride
+        self.windows = []
+        self.labels = []
     
     def __len__(self) -> int:
         """
@@ -25,14 +30,30 @@ class PMUDataset(Dataset):
         """
         return self.windows[idx], self.labels[idx]
 
-    def add_dataframe(self, df: pd.DataFrame) -> None:
+    def add_dataframe(self, df: pd.DataFrame, cols: list[str], name: str, set_num: int = None) -> None:
         """
         Adds an extra dataframe to the dataset, allows for gradual creation,
         avoiding high memory costs
+        Parameters:
+        df: dataframe to add
+        cols: the columns of the dataframe to add
+        name: name of pmu
+        set_num: the number dataset this is
         """
-        return
+        if set_num is None:
+            set_num = self.num_dataframes
+        
+        self.num_dataframes += 1
+        new_windows = self._create_windows(df, cols)
 
-    def _create_windows(self, df: pd.DataFrame) -> List(torch.Tensor):
+        self.windows.extend(new_windows)
+        self.labels.extend([set_num] * len(new_windows))
+        
+        self.pmu_source[set_num] = name
+
+        
+
+    def _create_windows(self, df: pd.DataFrame, cols: list[str]) -> List(torch.Tensor):
         """
         From a dataframe's data, creates the windows of data 
         """
